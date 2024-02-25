636bd36fbd697b918aa7892de959d7e3719fd7b8
zabey <melchakoff.sergey@yandex.ru>
2024-02-25 15:56:57 +0300

    add hw4.1

---
+++ hw4/Lesson 4.1/src/Main.java
@@ -0,0 +1,17 @@
+public class Main{
+    public static void main(String[] args) {
+        int a = 11;
+        int b = 5;
+        int myHome = 12;
+        int myHomies = 14;
+        int yourCar = 16;
+        String squash = "fourPounds";
+        char oneSymbol = '!';
+        char twoSymbol = '?';
+        int c = 10;
+        double d = 2.3;
+        System.out.println(a + " " + b + " " + myHome + " " + myHomies + " " + yourCar + " " + squash + " " + oneSymbol + " " + twoSymbol + " " + c + " " + d);
+
+
+    }
+}
