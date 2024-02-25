922e773833b9a90a1160d07c1707c11850246db3
zabey <melchakoff.sergey@yandex.ru>
2024-02-25 16:00:13 +0300

    add hw4.3

---
+++ hw4/Lesson 4.3/src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("****\tКалькулятор для расчета суммы, разности и частого\t****");
+        System.out.println("****\t\t\tВыберите категорию исчисления\t\t\t\t****");
+        System.out.println("****\t\t\t\t\t1 --- Сумма\t\t\t\t\t\t\t****");
+        System.out.println("****\t\t\t\t\t2 --- Разность\t\t\t\t\t\t****");
+        System.out.println("****\t\t\t\t\t3 --- Частное\t\t\t\t\t\t****");
+        int button = new Scanner(System.in).nextInt();
+        if(button == 1){
+            System.out.println("****\t\t\t\t\tКатегория: \"Сумма\t\t\t\t\t****");
+            System.out.println("****\t\t\t\tВведите первое число:\t\t\t\t\t****");
+            double firstNumber = new Scanner(System.in).nextDouble();
+            System.out.println("****\t\t\t\tВведите второе число:\t\t\t\t\t****");
+            double secondNumber = new Scanner(System.in).nextDouble();
+            System.out.println("****\t\t\t\t\t\tРезультат:\t\t\t\t\t\t****");
+            System.out.println(firstNumber + " " + "+" + " " +  secondNumber + " " + "=" + " " + (firstNumber+secondNumber));
+
+        } else if(button>3||button<1) {
+            System.out.println("****\t\t\tВы ввели неверную категорию!\t\t\t\t****\n****\tПерезапустите приложение и введите верную категорию\t****");
+        }
+
+        if(button == 2){
+            System.out.println("****\t\t\t\tКатегория: \"Разность\"\t\t\t\t\t****");
+            System.out.println("****\t\t\t\tВведите первое число:\t\t\t\t\t****");
+            double firstNumber = new Scanner(System.in).nextDouble();
+            System.out.println("****\t\t\t\tВведите второе число:\t\t\t\t\t****");
+            double secondNumber = new Scanner(System.in).nextDouble();
+            System.out.println("****\t\t\t\t\t\tРезультат:\t\t\t\t\t\t****");
+            System.out.println(firstNumber + " " + "-" + " " +  secondNumber + " " + "=" + " " + (firstNumber-secondNumber));
+        }
+        if(button == 3){
+            System.out.println("****\t\t\t\tКатегория: \"Частное\"\t\t\t\t\t****");
+            System.out.println("****\t\t\t\tВведите первое число:\t\t\t\t\t****");
+            double firstNumber = new Scanner(System.in).nextDouble();
+            System.out.println("****\t\t\t\tВведите второе число:\t\t\t\t\t****");
+            double secondNumber = new Scanner(System.in).nextDouble();
+            System.out.println("****\t\t\t\t\t\tРезультат:\t\t\t\t\t\t****");
+            System.out.println(firstNumber + " " + "/" + " " +  secondNumber + " " + "=" + " " + (firstNumber/secondNumber));
+        }
+    }
+}
